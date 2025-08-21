d5a7485a52c07663aa22a4ffdbefa3a02e75162b
Andrew Stroganov <savthe@gmail.com>
2025-08-21 23:07:15 +0300

    Move Trainset to tree_trainer

--- src/decision_tree/mod.rs
+++ src/decision_tree/mod.rs
@@ -1,5 +1,3 @@
-use argminmax::ArgMinMax;
-
 pub mod tree_classifier_impl;
 pub mod tree_regressor_impl;
 
@@ -7,10 +5,10 @@ pub mod tree_classifier;
 pub mod tree_regressor;
 
 mod decision_tree;
-mod trainset;
 mod tree_trainer;
+pub use tree_trainer::Trainset;
+
 use decision_tree::DecisionTree;
 
-pub use trainset::Trainset;
 pub use tree_classifier_impl::TreeClassifierImpl;
 pub use tree_regressor_impl::TreeRegressorImpl;
