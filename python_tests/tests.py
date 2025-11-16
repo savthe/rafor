c06d5289ab68bcb55f196209665279d06dae2a9c
savthe <savthe@gmail.com>
2025-11-16 23:24:53 +0300

    Regression tests (#14)
    Add regression dataset, python tests and rafor tests

--- python_tests/tests.py
+++ python_tests/tests.py
@@ -2,10 +2,9 @@ import pandas as pd
 import sys
 
 from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
-from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import f1_score, mean_squared_error
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
 def classifier_accuracy(u, v):
     u = list(u)
@@ -48,10 +47,11 @@ def random_forest_classifier_overfit_self():
 
 
 def split_dataset(X, y):
-    X_train = X.loc[X.index % 5 != 0]
-    X_pred = X.loc[X.index % 5 == 0]
-    y_train = y.loc[y.index % 5 != 0]
-    y_ref = y.loc[y.index % 5 == 0]
+    SPLIT_STEP = 5
+    X_train = X.loc[X.index % SPLIT_STEP != 0]
+    X_pred = X.loc[X.index % SPLIT_STEP == 0]
+    y_train = y.loc[y.index % SPLIT_STEP != 0]
+    y_ref = y.loc[y.index % SPLIT_STEP == 0]
     return X_train, y_train, X_pred, y_ref
 
 
@@ -92,13 +92,80 @@ def random_forest_binary_classification():
     print("F1-score:", f1_score(y_ref, y_pred, pos_label='h'))
 
 
+def decision_tree_regressor_overfit_self():
+    print(sys._getframe().f_code.co_name, end='. ')
+    df = pd.read_csv('../datasets/Folds5x2_pp.csv')
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    clf = DecisionTreeRegressor(max_depth=1000)
+    clf.fit(X, y)
+    y_pred = clf.predict(X)
+    print("Error:", mean_squared_error(y, y_pred))
+
+
+
+def decision_tree_regressor():
+    print(sys._getframe().f_code.co_name, end='. ')
+    df = pd.read_csv('../datasets/Folds5x2_pp.csv')
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    X_train, y_train, X_pred, y_ref = split_dataset(X, y)
+    clf = DecisionTreeRegressor(max_depth=10)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_pred)
+    print("Error:", mean_squared_error(y_ref, y_pred))
+
+
+
+def random_forest_regressor_overfit_self():
+    print(sys._getframe().f_code.co_name, end='. ')
+    df = pd.read_csv('../datasets/Folds5x2_pp.csv')
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    clf = RandomForestRegressor(n_estimators=25, max_depth=1000)
+    clf.fit(X, y)
+    y_pred = clf.predict(X)
+    print("Error:", mean_squared_error(y, y_pred))
+
+
+
+def random_forest_regressor(max_depth):
+    df = pd.read_csv('../datasets/Folds5x2_pp.csv')
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    X_train, y_train, X_pred, y_ref = split_dataset(X, y)
+    clf = RandomForestRegressor(max_depth=max_depth, n_estimators=100)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_pred)
+    print("Error:", mean_squared_error(y_ref, y_pred))
+
+
+def random_forest_regressor_depth5():
+    print(sys._getframe().f_code.co_name, end='. ')
+    random_forest_regressor(max_depth=5)
+
+
+def random_forest_regressor_depth10():
+    print(sys._getframe().f_code.co_name, end='. ')
+    random_forest_regressor(max_depth=10)
+
+
 def run_tests():
     decision_tree_classifier_overfit_self()
     decision_tree_classifier_depth10()
+
+    random_forest_binary_classification()
+
     random_forest_classifier_overfit_self()
     random_forest_classifier_depth10()
     random_forest_classifier_depth10_self()
-    random_forest_binary_classification()
+
+    decision_tree_regressor_overfit_self()
+    decision_tree_regressor()
+
+    random_forest_regressor_overfit_self()
+    random_forest_regressor_depth5()
+    random_forest_regressor_depth10()
 
 
 if __name__ == "__main__":
